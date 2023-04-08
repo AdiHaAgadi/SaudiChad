435808534ff4adb8c7a1ada8cc2d500aac58c550
AdiHaAgadi <adiefrat071103@gmail.com>
2023-04-08 23:32:06 +0300

    feat(): add command handler

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
-const { Client, GatewayIntentBits  } = require('discord.js');
+const Discord = require('discord.js');
 
-const secrets = require('./secrets/secret_data');
+const secrets = require('../src/secrets/secret_data');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
+const client = new Discord.Client({ 
+    intents: [
+        Discord.GatewayIntentBits.Guilds, 
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.DirectMessages,
+    ],
+    partials: [
+        Discord.Partials.Channel,
+    ],
+});
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('../src/commands/').filter((file) => file.endsWith('.js'));
+
+commandFiles.forEach((file) => {
+    const command = require(`../src/commands/${file}`);
+    
+    client.commands.set(command.name, command);
+});
 
 client.once('ready', () => {
-    console.log('SaudiChad is online!')
+    console.log('SaudiChad is online!\n')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content.startsWith(prefix) && !message.author.bot) {
+
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        try {
+            client.commands.get(command).execute(message, args);
+            console.log(`command '${command}' executed!`);
+        } catch (e) {
+            console.log(`command '${command}' does not exist!`);
+        }
+    }
 })
 
 client.login(secrets.Token);
