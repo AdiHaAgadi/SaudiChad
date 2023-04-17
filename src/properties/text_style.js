3aec502c24fe9591e4a453116181838a2d8a0ccd
AdiHaAgadi <adiefrat071103@gmail.com>
2023-04-17 18:28:46 +0300

    feat(src/properties/text_style.js): create file for text manipulation options in discord for easy use of bot

---
+++ src/properties/text_style.js
@@ -0,0 +1,7 @@
+module.exports = {
+    bold: (text) => `**${text}**`,
+    underline: (text) => `__${text}__`,
+    italic: (text) => `*${text}*`,
+    strikethrough: (text) => `~~${text}~~`,
+    spoiler: (text) => `||${text}||`,
+}
